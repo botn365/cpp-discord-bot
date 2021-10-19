3e1ba6ab0aa7e62a9b660304054af48240f45a44
botn365 <Krampus.sack.never@gmail.com>
2021-10-19 23:08:43 +0200

    compile on linux

---
+++ cmake/addSqlite3.cmake
@@ -0,0 +1,10 @@
+
+message(${CMAKE_SOURCE_DIR})
+set(SQLITE_INCLUDE ${CMAKE_SOURCE_DIR}/libs/sqlite/include)
+
+add_library(sqlite3 STATIC
+       ${SQLITE_INCLUDE}/sqlite3.c
+        ${SQLITE_INCLUDE}/sqlite3.h)
+
+find_package(Threads REQUIRED)
+target_link_libraries(sqlite3 INTERFACE Threads::Threads ${CMAKE_DL_LIBS})
