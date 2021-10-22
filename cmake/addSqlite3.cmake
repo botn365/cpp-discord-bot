9fc4ca4d524c5f761c270bc9118f71173b1bdc7d
botn365 <Krampus.sack.never@gmail.com>
2021-10-22 21:26:36 +0200

    add autodowload for rapidjson and sqlite

--- cmake/addSqlite3.cmake
+++ cmake/addSqlite3.cmake
@@ -1,8 +1,8 @@
-set(SQLITE_INCLUDE ${CMAKE_SOURCE_DIR}/libs/sqlite/include)
-
 add_library(sqlite3 STATIC
        ${SQLITE_INCLUDE}/sqlite3.c
         ${SQLITE_INCLUDE}/sqlite3.h)
 
-find_package(Threads REQUIRED)
+if (UNIX)
+    find_package(Threads REQUIRED)
+endif()
 target_link_libraries(sqlite3 INTERFACE Threads::Threads ${CMAKE_DL_LIBS})
