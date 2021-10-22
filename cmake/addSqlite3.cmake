d243ecd4c69cd35aa2f8f2fb722aa35f2d2b529a
bot <Krampus.sack.never@gmail.com>
2021-10-22 22:26:34 +0200

    get to compile on windows with autodownload

--- cmake/addSqlite3.cmake
+++ cmake/addSqlite3.cmake
@@ -2,7 +2,5 @@ add_library(sqlite3 STATIC
        ${SQLITE_INCLUDE}/sqlite3.c
         ${SQLITE_INCLUDE}/sqlite3.h)
 
-if (UNIX)
-    find_package(Threads REQUIRED)
-endif()
+find_package(Threads REQUIRED)
 target_link_libraries(sqlite3 INTERFACE Threads::Threads ${CMAKE_DL_LIBS})
